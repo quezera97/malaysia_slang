a4ef88fdb94df5c56b5d567c9130a710d7c402a5
LaptopMerah ver2.0 <zahir@mitsb.my>
2023-03-08 16:46:21 +0800

    image bendera

--- lib/ui/widget/selectCardSlang.dart
+++ lib/ui/widget/selectCardSlang.dart
@@ -9,20 +9,20 @@ class Choice {
 }
 
 const List<Choice> slangChoices = const <Choice>[
-  const Choice(title: 'K. Lumpur', image: 'assets/img/jata/kl.png'),
-  const Choice(title: 'Selangor', image: 'assets/img/jata/selangor.png'),
-  const Choice(title: 'Johor', image: 'assets/img/jata/johor.png'),
-  const Choice(title: 'N. Sembilan', image: 'assets/img/jata/nismilan.png'),
-  const Choice(title: 'Melaka', image: 'assets/img/jata/melaka.png'),
-  const Choice(title: 'Kelantan', image: 'assets/img/jata/kelantan.png'),
-  const Choice(title: 'Kedah', image: 'assets/img/jata/kedah.png'),
-  const Choice(title: 'Pahang', image: 'assets/img/jata/pahang.png'),
-  const Choice(title: 'Perak', image: 'assets/img/jata/perak.png'),
-  const Choice(title: 'Perlis', image: 'assets/img/bendera/perlis.png'),
-  const Choice(title: 'P. Pinang', image: 'assets/img/jata/penang.png'),
-  const Choice(title: 'Sabah', image: 'assets/img/jata/sabah.png'),
-  const Choice(title: 'Sarawak', image: 'assets/img/jata/sarawak.png'),
-  const Choice(title: 'Terengganu', image: 'assets/img/jata/terengganu.png'),
+  const Choice(title: 'K. Lumpur', image: 'assets/img/kl.png'),
+  const Choice(title: 'Selangor', image: 'assets/img/selangor.png'),
+  const Choice(title: 'Johor', image: 'assets/img/johor.png'),
+  const Choice(title: 'N. Sembilan', image: 'assets/img/nismilan.png'),
+  const Choice(title: 'Melaka', image: 'assets/img/melaka.png'),
+  const Choice(title: 'Kelantan', image: 'assets/img/kelantan.png'),
+  const Choice(title: 'Kedah', image: 'assets/img/kedah.png'),
+  const Choice(title: 'Pahang', image: 'assets/img/pahang.png'),
+  const Choice(title: 'Perak', image: 'assets/img/perak.png'),
+  const Choice(title: 'Perlis', image: 'assets/img/perlis.png'),
+  const Choice(title: 'P. Pinang', image: 'assets/img/penang.png'),
+  const Choice(title: 'Sabah', image: 'assets/img/sabah.png'),
+  const Choice(title: 'Sarawak', image: 'assets/img/sarawak.png'),
+  const Choice(title: 'Terengganu', image: 'assets/img/terengganu.png'),
 ];
 
 class SelectCardSlang extends StatelessWidget {
@@ -39,9 +39,8 @@ class SelectCardSlang extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       child: InkWell(
         onTap: () {
-          if (choice.title == CardEnum.restaurant) {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => Restaurant()));
+          if (choice.title == 'Selangor') {
+            print('lol');
           }
         },
         child: Padding(
@@ -53,7 +52,7 @@ class SelectCardSlang extends StatelessWidget {
                 FittedBox(
                   child: Image.asset(choice.image),
                   fit: BoxFit.fill,
-                  ),
+                ),
                 Text(choice.title,
                   style: TextStyle(
                     fontSize: 15,
