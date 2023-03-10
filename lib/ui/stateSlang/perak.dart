71b46a497f1a80d6ccbb360dbda3983f5171fd48
LaptopMerah ver2.0 <zahir@mitsb.my>
2023-03-10 17:11:16 +0800

    Perak, Perlis, Sabah, Sarawak

--- lib/ui/stateSlang/perak.dart
+++ lib/ui/stateSlang/perak.dart
@@ -1,6 +1,4 @@
 class PerakList {
   static List<String> perak = <String>[
-    'rak? + Hotel mana eh yang ok? + sound/restaurant/test.mp3',
-    'Baasdsbi? + Hotel mana eh yang ok? + sound/restaurant/test.mp3',
   ];
 }
